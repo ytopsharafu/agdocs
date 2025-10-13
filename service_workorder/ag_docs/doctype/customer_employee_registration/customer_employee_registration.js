c39dcbad099fd4e1c192cd2e573daa553745ed52
ytopsharafu <ytopsharafu@gmail.com>
2025-10-13 11:40:26 +0400

    agdocs

---
+++ service_workorder/ag_docs/doctype/customer_employee_registration/customer_employee_registration.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Mohamed Sharafudheen and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Customer Employee Registration", {
+// 	refresh(frm) {
+
+// 	},
+// });
